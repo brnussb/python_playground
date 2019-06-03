3f0a06c78a461089dbd8c55b4a81e547cf2368c7
Bryan Nussbaum <bryan.nussbaum.h6p2@gmail.com>
2019-06-02 21:30:21 -0500

    updating the P0 project

--- P0/Task0.py
+++ P0/Task0.py
@@ -7,11 +7,22 @@ with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
+firsttext = texts[0]
+incoming_number, answering_number, time = firsttext[0], firsttext[1], firsttext[2]
+
+
+print(firsttext)
+print ("First record of texts, {} texts {} at time {}.".format(incoming_number, answering_number, time))
+
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-print(reader)
+lastcall = calls[-1]
+
+print(lastcall)
+print("Last record of calls, {} calls {} at time {}, lasting {} seconds.".format('incoming number', 'answering number', 'time', 'during'))
+
 """
 TASK 0:
 What is the first record of texts and what is the last record of calls?
@@ -19,4 +30,3 @@ Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
-
